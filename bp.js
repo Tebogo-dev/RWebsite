535016e6d29828488e2100343c49d63619e562bb
Tebogo <94841126+Tebogo-dev@users.noreply.github.com>
2024-01-06 18:00:36 +0200

    Update bp.js

--- bp.js
+++ bp.js
@@ -4,7 +4,7 @@ console.dir(document);
 var sign_up = document.querySelector('.field_form');
 var lets_connect = document.querySelector('.connect');
 var close_menu = document.querySelector('.close');
-var background = document.querySelector('.entire_page');
+var back = document.querySelector('.entire_page');
 
 lets_connect.addEventListener('click', show);
 close_menu.addEventListener('click', close);
@@ -12,19 +12,19 @@ close_menu.addEventListener('click', close);
 function show() {
     sign_up.style.display = 'block';  
     sign_up.style.left = '1';
-    background.style.opacity = '0.3';
+    back.style.opacity = '0.3';
     toggle_off = 'disable';
 }
 function close () {
     sign_up.style.display = 'none';
-    background.style.opacity = '1';
+    back.style.opacity = '1';
 }
 
 
 window.onclick = function(event) {
     if (event.current == 'show') {
         sign_up.style.display = 'none';
-        background.style.opacity = '1';
+        back.style.opacity = '1';
     }
 }
 
